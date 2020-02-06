d97f765779224f9a587edaed6f94a1a68a3d63e2
JessePel <39877962+Codingman11@users.noreply.github.com>
2020-02-06 21:30:41 +0200

    Tehtava 1

--- Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.viikko7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+    }
+
+    public void hello(View v) {
+        System.out.println("Hello World!");
     }
 }
