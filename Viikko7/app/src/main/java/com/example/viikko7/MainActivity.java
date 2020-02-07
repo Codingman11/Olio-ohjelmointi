a1b5172f46a8bd00bc73ebc4dec4e3d51c233db5
JessePel <jesse1.peltola@outlook.com>
2020-02-07 17:58:03 +0200

    Vko7_3

--- Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -4,22 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        text = (TextView) findViewById(R.id.textView);
+         setContentView(R.layout.activity_main);
+
 
     }
 
-    public void hello(View v) {
-        text.setText("Hello World!");
+    public void changeText(View v) {
+
+        TextView text = (TextView) findViewById(R.id.textView);
+        EditText textEdit = (EditText) findViewById(R.id.editText2);
+        text.setText(textEdit.getText());
 
     }
 
