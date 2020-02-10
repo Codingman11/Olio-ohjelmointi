27d8720a67e882df307faf9f4423952fa23a6973
JessePel <jesse1.peltola@outlook.com>
2020-02-10 11:16:37 +0200

    Vko7_4

--- Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -3,27 +3,51 @@ package com.example.viikko7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    private EditText textEdit;
+    private TextView text_view;
+    private TextWatcher text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_main);
-
+         changeText();
 
     }
 
-    public void changeText(View v) {
+    public void changeText() {
 
-        TextView text = (TextView) findViewById(R.id.textView);
-        EditText textEdit = (EditText) findViewById(R.id.editText2);
-        text.setText(textEdit.getText());
+        textEdit = (EditText) findViewById(R.id.textEdit);
+        text_view = (TextView) findViewById(R.id.textView);
+
+
+        text = new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                text_view.setText(textEdit.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        textEdit.addTextChangedListener(text);
+
 
     }
 
