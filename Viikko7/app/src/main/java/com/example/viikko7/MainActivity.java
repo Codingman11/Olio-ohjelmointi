3bbe7e9943264a2b6fdb78b528aa2b25b15f8d09
JessePel <jesse1.peltola@outlook.com>
2020-02-10 13:37:34 +0200

    Vko7_5

--- Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -2,53 +2,88 @@ package com.example.viikko7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+
 import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.InputStream;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText textEdit;
     private TextView text_view;
-    private TextWatcher text;
+    private Object InputStream;
+    private EditText file;
+    Context context = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_main);
-         changeText();
+         context = MainActivity.this;
+
+
 
     }
 
-    public void changeText() {
+
+    public void readFile(View v) {
+
+        text_view = (TextView) findViewById(R.id.viewText);
+        file = (EditText) findViewById(R.id.fileName);
+
+        try {
+            InputStream ins = context.openFileInput(file.getText().toString());
+            BufferedReader br = new BufferedReader(new InputStreamReader(ins));
+
+            String text = "", line = "";
+
+            while ((line = br.readLine()) != null) {
+                text += line;
+            }
+            text_view.setText(text);
+            br.close();
+        } catch (IOException e) {
+            Log.e("IOException", "Virhe");
+        } finally {
+            System.out.println("Luettu");
+        }
+    }
+
+
+
+    public void writeFile(View v) {
 
         textEdit = (EditText) findViewById(R.id.textEdit);
-        text_view = (TextView) findViewById(R.id.textView);
+        file = (EditText) findViewById(R.id.fileName);
+        String text = "";
+        try {
 
+            OutputStreamWriter ows = new OutputStreamWriter(context.openFileOutput(file.getText().toString(), Context.MODE_PRIVATE));
 
-        text = new TextWatcher() {
+            text += textEdit.getText().toString();
+            ows.write(text);
+            System.out.println("Tiedoston nimi: " + file.getText().toString());
 
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            ows.close();
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                text_view.setText(textEdit.getText().toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        };
-
-        textEdit.addTextChangedListener(text);
-
+        } catch (IOException e) {
+            Log.e("IOException", "Virhe");
+        } finally {
+            System.out.println("Kirjoitettu");
+        }
 
     }
+
 
 }
