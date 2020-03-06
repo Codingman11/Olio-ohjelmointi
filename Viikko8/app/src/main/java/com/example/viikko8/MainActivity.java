07d450b65f084c6d40a5ffbcc601e72a1df452ef
JessePel <39877962+Codingman11@users.noreply.github.com>
2020-03-06 17:50:38 +0200

    Viikko 8 tehtava 1, mutta ohjelma ei toimi android studiolla mutta NetBeansissa toimii kyllä

--- Viikko8/app/src/main/java/com/example/viikko8/MainActivity.java
+++ Viikko8/app/src/main/java/com/example/viikko8/MainActivity.java
@@ -4,11 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Mainclass();
+    }
+
+    public void Mainclass () {
+        BottleDispenser boDis = BottleDispenser.getInstance();
+        int choice = -1;
+        while (choice != 0) {
+            System.out.println("\n*** BOTTLE DISPENSER ***");
+            System.out.println("1) Add money to the machine");
+            System.out.println("2) Buy a bottle");
+            System.out.println("3) Take money out");
+            System.out.println("4) List bottles in the dispenser");
+            System.out.println("0) End");
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    boDis.addMoney();
+                    break;
+                case 2:
+                    boDis.listMachine();
+                    System.out.print("Your choice: ");
+                    int ch = sc.nextInt();
+                    boDis.buyBottle(ch);
+                    break;
+                case 3:
+                    boDis.returnMoney();
+                    break;
+                case 4:
+                    boDis.listMachine();
+                    break;
+                case 0:
+                    choice = 0;
+                    break;
+            }
+
+        }
     }
 }
