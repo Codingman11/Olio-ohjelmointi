f5819e25a3a8b2ca2878d6ad3920f8530a5c698b
JessePel <39877962+Codingman11@users.noreply.github.com>
2020-04-06 11:09:00 +0300

    Viikko 9 tehtävä 2

---
+++ Viikko9/app/src/main/java/com/example/viikko9/Posti.java
@@ -0,0 +1,36 @@
+package com.example.viikko9;
+
+public class Posti extends smartPost{
+
+    private String id;
+    private String name;
+    private String city;
+    private String address;
+    private String avail;
+
+    public Posti(String id, String name, String city, String address, String avail) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.address = address;
+        this.avail = avail;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+
+}
